ffa99924a0fca7b9759bac0ea3ad57248a9a3644
Novojit Saha <novojitsaha@hotmail.com>
2024-10-06 15:06:39 +0200

    Update MatchSelection with competition dropdown

--- src/components/MatchSelection.tsx
+++ src/components/MatchSelection.tsx
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
-import competitions from "./assets/competitions/competitions.json";
+import competitions from "../assets/competitions/competitions.json";
 
 const MatchSelection = () => {
 
-    const competitionList: string[] = Array.from(new Set(competitions.map((item: any) => item.competition_name)));
-    
+    // State to track competition
     const [competition, setCompetition] = useState<string>("");
+
+    // State to track season_name for each competition
+    const [season, setSeason] = useState<string>("");
+
+    // set of available competition names
+    const competitionList: string[] = Array.from(new Set(competitions.map((item: any) => item.competition_name)));
+
+    // list of available season_id for each competition: {competition_name: [season_name]}
+    const competitionSeasons: {[key: string]: string[]} = {};
+
+    // initialize the keys for competitionSeasons
+    competitionList.map((competition_name) => competitionSeasons[competition_name] = [])
+
+    // populate competitionSeasons
+    competitions.forEach((item : any) => {
+      const competitionName: string = item.competition_name;
+      const seasonName: string = item.season_name;
+      competitionSeasons[competitionName].push(seasonName);
+    })
+
+
+
+
+
+
 
     const handleCompetition = (event: any) => {
         setCompetition(event.target.value);
@@ -16,11 +40,12 @@ const MatchSelection = () => {
       <div className="flex border border-black m-2 justify-center">
         <label className="m-2 font-bold">Select match:</label>
         <div className="">
-          <select id="competitions" className="m-2" value={competition} onChange={handleCompetition}>
+          <select id="competitionSelection" className="m-2" value={competition} onChange={handleCompetition}>
             <option value="" disabled>Select Competition</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
+            {
+              competitionList.map((competition_name) => <option value={competition_name} key={competition_name}>{competition_name}</option>)
+            }
+
           </select>
 
           <select id="competitions" className="m-2">
